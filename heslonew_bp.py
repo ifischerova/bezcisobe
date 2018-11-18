1601d1c94f5b71981e4fb8afb7154f37cfd02193
Iva <44750700+Ivuska@users.noreply.github.com>
2018-11-18 08:29:58 +0100

    Obnova hesla, pridani auta, urpava html

--- heslonew_bp.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template
-blueprint = Blueprint('heslonew_bp', __name__)
-@blueprint.route('/noveheslo')
-def show_heslonew():
-	return render_template('heslonew.html')
--- kdo_co_bp.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template
-blueprint = Blueprint('kdo_co_bp', __name__)
-@blueprint.route('/kdoco')
-def show_kdo_co():
-	return render_template('kdo_co.html')
