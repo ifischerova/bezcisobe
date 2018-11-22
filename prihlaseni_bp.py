6ac70d738ddb9b601de88f627b982e33b68be4a5
Petr Krebs <petr-k@users.noreply.github.com>
2018-11-22 21:00:45 +0100

    prihlasovani
    Co-authored-by: petrusakn <44819702+petrusakn@users.noreply.github.com>

--- prihlaseni_bp.py
+++ prihlaseni_bp.py
@@ -1,37 +1,39 @@
 from flask import Blueprint, current_app, request, render_template, redirect, url_for
 # from myapp.models import User
+from hashlib import sha512
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 from wtforms import Form, TextField, PasswordField, validators
 # https://infinidum.com/2018/08/18/making-a-simple-login-system-with-flask-login/
 # pip install flask-wtf flask-login
+from db_funkce import najdi_uzivatele
 
 class LoginForm(Form):
-    email = TextField('E-mail', [validators.Required(), validators.Length(min=4, max=25)])
-    password = PasswordField('Heslo', [validators.Required(), validators.Length(min=6, max=200)])
+    email = TextField('email', [validators.Required(), validators.Length(min=4, max=25)])
+    password = PasswordField('heslo', [validators.Required(), validators.Length(min=6, max=200)])
 
-class User(UserMixin):
-    def __init__(self,id):
-        self.id = id
-
-#@login_manager.user_loader
-#def load_user(user_id):
-#    return User(user_id)
 
 blueprint = Blueprint('prihlaseni_bp', __name__)
-@blueprint.route('/prihlaseni')
-def show_prihlaseni():
-    return render_template('prihlaseni.html')
-
 
 @blueprint.route('/prihlaseni', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
-    error = None
-    if request.method == 'POST' and form.validate():
-        user = User.query.filter_by(username=username.lower()).first()
-        if user:
-            if login_user(user):
-                current_app.logger.debug('Logged in user %s', user.username)
-                return redirect(url_for('secret'))
-        error = 'Invalid username or password.'
-    return render_template('login.html', form=form, error=error)
+    chyba = None
+    if request.method == 'POST':
+        # TODO: validace poli formulare
+
+        email = request.form["email"]
+        heslo = request.form["heslo"]
+        uzivatel = najdi_uzivatele(email)
+        uspesne_prihlasen = False
+        if uzivatel:
+            if sha512(heslo.encode()).hexdigest() == uzivatel.password_hash:
+                if login_user(uzivatel, force=True):
+                    uspesne_prihlasen = True
+        
+        if not uspesne_prihlasen:
+            chyba = "Neplatné přihlašovací údaje"
+
+    # TODO: zobrazit upozorneni o spatne zadanem jmenu/heslu
+    # TODO: presmerovani po uspesnem prihlaseni - kam?
+    # TODO: při neúspěšném pokusu o přihlášení zachovat ve formuláři zadaný email
+    return render_template('prihlaseni.html', form=form, chyba=chyba)
