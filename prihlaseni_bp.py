d20283c07dd76d41df81d9b49a7480e956ab4aab
petrusakn <44819702+petrusakn@users.noreply.github.com>
2018-12-04 19:53:38 +0100

    Oprava problemu s presmerovanim z registrace a mizejici krizek u hlasek

--- prihlaseni_bp.py
+++ prihlaseni_bp.py
@@ -57,7 +57,7 @@ def login():
             elif sha512(heslo.encode()).hexdigest() == uzivatel.password_hash:
                 if login_user(uzivatel, force=True):
                     flash ('Uživatel byl úspěšně přihlášen.', "success")
-                    if next.endswith('prihlaseni'):
+                    if next.endswith('prihlaseni') or next.endswith('registrace'):
                         return redirect(url_for('zavody_bp.show_zavody'))
                     return redirect(next)
         if not uzivatel:
