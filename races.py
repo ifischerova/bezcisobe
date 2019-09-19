b09ba2260bdf909261f19989f58452f658825bcf
fischerova <iva.fischerova@avast.com>
2019-09-19 12:50:31 +0200

    added appropriate comments replaced if...else conditions with dictionary

--- races.py
+++ races.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup as soup 
 from urllib.request import urlopen as uReq
-import win_unicode_console # potřebuji, protože mám špatné kódování
-win_unicode_console.enable() # potřebuji, protože mám špatné kódování
+import win_unicode_console
+#I need import this because I have s problem with encoding in my notebook
+win_unicode_console.enable()
+#I need import this because I have s problem with encoding in my notebook
+
 
 page_list = list(range(0, 91))
 
@@ -18,75 +21,105 @@ for p in page_list:
     uClient.close()
     page_soup = soup(page_html, "html.parser")
 
+
+    # This function find list with 35 items, which have selector/locator "class = row".
+    # Unfortunately I am not able to find more relevant selector in the web page to find a specific container with races.
+    # print(len(containers))
     containers = page_soup.findAll("div", { "class" : "row"})
-#najde seznam o 35 prvcích
-#print(len(containers))
 
+    # I found by trial-error method that container with relevant content is the 12th one.
+    # So I need to select container with index 11.
+    # print(container)
     container = containers[11]
-#print(container)
 
+    # This function will find a list of items, which contains two kinds of span with "class = text-muted iframe-hidden".
+    # First is for date of the race, the second for the place of the race.
     date_place = container.findAll("span", {"class" : "text-muted iframe-hidden"})
+    # I decided to take a place of the race from another (for me more suitable part) of the webpage, so right now
+    # I need only each second item from the list (starting from the first item).
+    # I decided delete every second item from the list (starting from the second item).
     del date_place[1::2]
-    date = date_place
 
-    for d in date:
-        datum_zavodu_se_dny = d.text
-        rozsekane_datum = datum_zavodu_se_dny.split(' ')
+    dates = date_place
 
-        den_jedna_cifra = int(rozsekane_datum[1].strip('.'))
-        if den_jedna_cifra < 10:
-            den = '0' + str(den_jedna_cifra)
+    for d in dates:
+        d = dates[0]
+        date_with_days = d.text
+        sliced_date = date_with_days.split(' ')
+
+        day_with_one_digit = int(sliced_date[1].strip('.'))
+        if day_with_one_digit < 10:
+            day = '0' + str(day_with_one_digit)
         else:
-            den = str(den_jedna_cifra)
+            day = str(day_with_one_digit)
 
-        mesic_slovo = rozsekane_datum[2]
-        if mesic_slovo == 'ledna':
-            rozsekane_datum[2] = '01'
-        elif mesic_slovo == 'února':
-            rozsekane_datum[2] = '02'
-        elif mesic_slovo == 'března':
-            rozsekane_datum[2] = '03'
-        elif mesic_slovo == 'dubna':
-            rozsekane_datum[2] = '04'
-        elif mesic_slovo == 'května':
-            rozsekane_datum[2] = '05'
-        elif mesic_slovo == 'června':
-            rozsekane_datum[2] = '06'
-        elif mesic_slovo == 'července':
-            rozsekane_datum[2] = '07'
-        elif mesic_slovo == 'srpna':
-            rozsekane_datum[2] = '08'
-        elif mesic_slovo == 'září':
-            rozsekane_datum[2] = '09'
-        elif mesic_slovo == 'října':
-            rozsekane_datum[2] = '10'
-        elif mesic_slovo == 'listopadu':
-            rozsekane_datum[2] = '11'
-        elif mesic_slovo == 'prosince':
-            rozsekane_datum[2] = '12'
+        #Because of our database I need to change names of months to numbers, 
+        #starting with 0, if the number of the month is smaller than 10. At first I made it with a condition,
+        #but it is a quite long and same code. So I tried to make it through dictionary.
+        #Keys of dictionary are names of months in the form of the word the same as at the page.
+        names_of_months = ['ledna', 'února', 'března', 'dubna', 'května', 'června', 'července', 'srpna', 'září', 'října', 'listopadu', 'prosince']
+        #print(names_of_months)
+        #values are numbers for months, for example january = 1, etc.
+        values_of_months = list(range(1, 13))
+        #print( values_of_months)
+        months = dict(zip(names_of_months,  values_of_months))
+        #print(months)
+        month = sliced_date[2]
+        if month in months:
+            month = '0' + str(months[month])
+            #print(month)
         else:
-            print("Tady není žádný měsíc!")
+            print("U závodu není uvedeno datum v potřebném formátu.")
 
-        rok = rozsekane_datum[3].replace(',', ' ').strip()
-        rozsekane_datum[1] = rok
-        mesic = rozsekane_datum[2]
-        rozsekane_datum[3] = den
-        jen_datum = rozsekane_datum[1:4]
-        datum_zavodu = '-'.join(jen_datum)
-        print(datum_zavodu)
-        #f.write(datum_zavodu)
+        #Original way how I was trying to change names of months to its values.
+        #if month_in_word == 'ledna':
+            #sliced_date[2] = '01'
+        #elif month_in_word == 'února':
+            #sliced_date[2] = '02'
+        #elif month_in_word == 'března':
+            #sliced_date[2] = '03'
+        #elif month_in_word == 'dubna':
+            #sliced_date[2] = '04'
+        #elif month_in_word == 'května':
+            #sliced_date[2] = '05'
+        #elif month_in_word == 'června':
+            #sliced_date[2] = '06'
+        #elif month_in_word == 'července':
+            #sliced_date[2] = '07'
+        #elif month_in_word == 'srpna':
+            #sliced_date[2] = '08'
+        #elif month_in_word == 'září':
+            #sliced_date[2] = '09'
+        #elif month_in_word == 'října':
+            #sliced_date[2] = '10'
+        #elif month_in_word == 'listopadu':
+            #sliced_date[2] = '11'
+        #elif month_in_word == 'prosince':
+            #sliced_date[2] = '12'
+        #else:
+            #print("There is no month!")
+
+        year = sliced_date[3].replace(',', ' ').strip()
+        sliced_date[1] = year
+        sliced_date[2] = month
+        sliced_date[3] = day
+        # '-'.join(only_date) - previous used, not so understadable?
+        #only_date = sliced_date[1:4]
+        # the name of the column in our database in PostgreSQL should be the same -> "date_of_race" instead of "datum_zavodu"
+        date_of_race = year + '-' + month + '-' + day
+        print(date_of_race)
+        #f.write(date_of_race)
 
     place = container.findAll("p", {"class" : "iframe-visible cb-iframe-place"})
     for p in place:
-        misto_zavodu = p.text.strip('()')
-        print(misto_zavodu)
-        #f.write(misto_zavodu)
+        # the name of the column in our database in PostgreSQL should be the same -> "place_of_race" instead of "misto_zavodu"
+        place_of_race = p.text.strip('()')
+        print(place_of_race)
+        #f.write(place_of_race)
 
     name = container.findAll("h4", {"class": "mt-0 mb-0"})
     for n in name:
-        nazev_zavodu = n.text.strip()
-        print(nazev_zavodu)
-        #f.write(nazev_zavodu)
-
-    #f.close()
-
+        #the name of the column in our database in PostgreSQL should be the same -> "name_of_race" instead of "nazev_zavodu"
+        name_of_race = n.text.strip()
+        print(name_of_race)
+        #f.write(name_of_race)
