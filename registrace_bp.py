6baadddaefc28459416ccc60420c822589165d08
fischerova <iva.fischerova@avast.com>
2019-09-24 17:32:16 +0200

    port away from deprecated wtforms validators.Required

--- registrace_bp.py
+++ registrace_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, abort, current_app
-from wtforms import Form, BooleanField, StringField, PasswordField, IntegerField, validators
+from wtforms import BooleanField, StringField, PasswordField, IntegerField, validators
 from flask_wtf import FlaskForm
 from flask_login import current_user
 import db_funkce
@@ -7,23 +7,23 @@ import db_funkce
 
 class RegistrationForm(FlaskForm):
     username = StringField(
-        'Jméno*:', [validators.Required(message='Bez jména to nepůjde.')])
+        'Jméno*:', [validators.DataRequired(message='Bez jména to nepůjde.')])
     surname = StringField(
-        'Příjmení*:', [validators.Required(message='Bez příjmení to nepůjde.')])
+        'Příjmení*:', [validators.DataRequired(message='Bez příjmení to nepůjde.')])
     street = StringField('Ulice,č.p./č.o.:')
     city = StringField(
-        'Město/Obec*:', [validators.Required(message='Vyplň město, kde bydlíš.')])
-    postcode = IntegerField('PSČ*:', [validators.Required(message='Bez PSČ to nepůjde.'),
+        'Město/Obec*:', [validators.DataRequired(message='Vyplň město, kde bydlíš.')])
+    postcode = IntegerField('PSČ*:', [validators.DataRequired(message='Bez PSČ to nepůjde.'),
                                       validators.Length(min=5, max=5, message='PSČ zadej bez mezer.')])
-    email = StringField('E-mail*:', [validators.Required(message='Bez mailu to nepůjde.', ),
+    email = StringField('E-mail*:', [validators.DataRequired(message='Bez mailu to nepůjde.', ),
                                      validators.Email(message='Chybný tvar emailové adresy.'), validators.Length(message="Email musí mít mezi 4 a 250 znaky.", min=4, max=250)])
-    phone = StringField('Telefon*:', [validators.Required(message='Bez telefonního čísla to nepůjde.'), validators.Length(
+    phone = StringField('Telefon*:', [validators.DataRequired(message='Bez telefonního čísla to nepůjde.'), validators.Length(
         min=11, max=11, message='Telefonní číslo zadej bez předvolby a ve tvaru xxx xxx xxx.')])
-    password = PasswordField('Heslo*:', [validators.Required(message='Bez hesla to nepůjde.'), validators.EqualTo(
+    password = PasswordField('Heslo*:', [validators.DataRequired(message='Bez hesla to nepůjde.'), validators.EqualTo(
         'confirm_password', message='Hesla se musejí shodovat.'), validators.Length(min=8, message='Heslo musí být alespoň 6 znaků dlouhé.')])
     confirm_password = PasswordField(
-        'Potvrzení hesla*:', [validators.Required(message='Zadané heslo je třeba potvrdit.')])
-    gdpr = BooleanField('Souhlasím ze zpracováním osobních údajů.*', [validators.Required(
+        'Potvrzení hesla*:', [validators.DataRequired(message='Zadané heslo je třeba potvrdit.')])
+    gdpr = BooleanField('Souhlasím ze zpracováním osobních údajů.*', [validators.DataRequired(
         message='Bez udělení Tvého souhlasu Tě nemůžeme zaregistrovat.')])
 
 
